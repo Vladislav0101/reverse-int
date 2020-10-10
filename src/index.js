d884b5076f674434dcabf77ffc2ab100e259020c
Vladislav0101 <by.vlad01@mail.ru>
2020-10-10 16:47:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n % 10 === 0) {
+        while (n % 10 === 0) {
+            n /= 10;
+        }
+    }
+    n = n < 0 ? -n : n;
+    return String(n).split('').reverse().join('');
 }
